abb5c6a0de0ebf7991d77d8c49e868a570092809
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-01 13:24:48 -0400

    Array generation working.

--- gen.py
+++ gen.py
@@ -1,26 +1,79 @@
-from win32 import Listener
+from pynput.mouse import Listener
+import numpy as np
+import random
 
+# Expand mode and Repeat mode.
+REPEAT = True
+TIME_STEPS = 1000
+coord_list = []
+index = 0
+start = 0
+end = 0
+
+"""
+We use global variables to avoid having
+to peer into ``pynput`` and mess with their
+``on_<event>`` functions. 
+"""
 def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+    global coord_list
+    global index
+    coord_list.append((x, y))
+    index += 1
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    if not pressed:
+    global coord_list
+    global index
+    global start
+    global end
+    if pressed:
+        coord_list.append((x, y))
+        index += 1
+        start = index
+    else:
+        coord_list.append((x, y))
+        index += 1
+        end = index
         # Stop listener
         return False
-
-def on_scroll(x, y, dx, dy):
-    print('Scrolled {0}'.format(
-        (x, y)))
 
 # Collect events until released
 with Listener(
         on_move=on_move,
         on_click=on_click,
-        on_scroll=on_scroll,
-        suppress=False, 
-        log_path='log_file.log') as listener:
+        on_scroll=None,
+        suppress=False) as listener:
     listener.join()
+
+coords = np.array(coord_list)
+raw_steps = coords.shape[0]
+coords = coords.astype(float)
+
+if REPEAT:
+    full_reps = TIME_STEPS // raw_steps
+    reps = [coords] * full_reps
+    coords = np.concatenate(reps)
+    raw_steps = coords.shape[0]
+    print("Shape after concat1:", coords.shape)
+
+while REPEAT and raw_steps < TIME_STEPS:
+    diff = TIME_STEPS - raw_steps
+    coords = np.concatenate([coords, coords[:diff]])
+    raw_steps = coords.shape[0]
+    print("Shape after concat2:", coords.shape)
+
+while not REPEAT and raw_steps < TIME_STEPS:
+    add_loc = random.randint(0,raw_steps - 2) # Inclusive right index. 
+    p1 = coords[add_loc]
+    p2 = coords[add_loc + 1]
+    mean = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+    coords = np.insert(coords, add_loc + 1, mean, 0)
+    raw_steps = coords.shape[0]
+
+while raw_steps > TIME_STEPS:
+    del_loc = random.randint(0,raw_steps - 1)
+    coords = np.delete(coords, del_loc, 0)
+    raw_steps = coords.shape[0]
+
+print("coords shape:", coords.shape)
+print(coords)
