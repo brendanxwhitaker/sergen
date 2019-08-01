c0b796ad5aef547df826f5df7347e3631424d6a8
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-01 12:31:44 -0400

    Base and win32 useless.

--- base.py
+++ base.py
@@ -27,7 +27,166 @@ is located in a platform dependent module.
 import enum
 
 from pynput._util import AbstractListener
-from pynput import _logger
+
+class Button(enum.Enum):
+    """The various buttons.
+
+    The actual values for these items differ between platforms. Some
+    platforms may have additional buttons, but these are guaranteed to be
+    present everywhere.
+    """
+    #: An unknown button was pressed
+    unknown = 0
+
+    #: The left button
+    left = 1
+
+    #: The middle button
+    middle = 2
+
+    #: The right button
+    right = 3
+
+
+class Controller(object):
+    """A controller for sending virtual mouse events to the system.
+    """
+    def __init__(self):
+        self._log = _logger(self.__class__)
+
+    @property
+    def position(self):
+        """The current position of the mouse pointer.
+
+        This is the tuple ``(x, y)``, and setting it will move the pointer.
+        """
+        return self._position_get()
+
+    @position.setter
+    def position(self, pos):
+        self._position_set(pos)
+
+    def scroll(self, dx, dy):
+        """Sends scroll events.
+
+        :param int dx: The horizontal scroll. The units of scrolling is
+            undefined.
+
+        :param int dy: The vertical scroll. The units of scrolling is
+            undefined.
+
+        :raises ValueError: if the values are invalid, for example out of
+            bounds
+        """
+        self._scroll(dx, dy)
+
+    def press(self, button):
+        """Emits a button press event at the current position.
+
+        :param Button button: The button to press.
+        """
+        self._press(button)
+
+    def release(self, button):
+        """Emits a button release event at the current position.
+
+        :param Button button: The button to release.
+        """
+        self._release(button)
+
+    def move(self, dx, dy):
+        """Moves the mouse pointer a number of pixels from its current
+        position.
+
+        :param int x: The horizontal offset.
+
+        :param int dy: The vertical offset.
+
+        :raises ValueError: if the values are invalid, for example out of
+            bounds
+        """
+        self.position = tuple(sum(i) for i in zip(self.position, (dx, dy)))
+
+    def click(self, button, count=1):
+        """Emits a button click event at the current position.
+
+        The default implementation sends a series of press and release events.
+
+        :param Button button: The button to click.
+
+        :param int count: The number of clicks to send.
+        """
+        with self as controller:
+            for _ in range(count):
+                controller.press(button)
+                controller.release(button)
+
+    def __enter__(self):
+        """Begins a series of clicks.
+
+        In the default :meth:`click` implementation, the return value of this
+        method is used for the calls to :meth:`press` and :meth:`release`
+        instead of ``self``.
+
+        The default implementation is a no-op.
+        """
+        return self
+
+    def __exit__(self, exc_type, value, traceback):
+        """Ends a series of clicks.
+        """
+        pass
+
+    def _position_get(self):
+        """The implementation of the getter for :attr:`position`.
+
+        This is a platform dependent implementation.
+        """
+        raise NotImplementedError()
+
+    def _position_set(self, pos):
+        """The implementation of the setter for :attr:`position`.
+
+        This is a platform dependent implementation.
+        """
+        raise NotImplementedError()
+
+    def _scroll(self, dx, dy):
+        """The implementation of the :meth:`scroll` method.
+
+        This is a platform dependent implementation.
+        """
+        raise NotImplementedError()
+
+    def _press(self, button):
+        """The implementation of the :meth:`press` method.
+
+        This is a platform dependent implementation.
+        """
+        raise NotImplementedError()
+
+    def _release(self, button):
+        """The implementation of the :meth:`release` method.
+
+        This is a platform dependent implementation.
+        """
+        raise NotImplementedError()
+
+def _logger(cls, log_path):
+    """Creates a logger with a name suitable for a specific class.
+    This function takes into account that implementations for classes reside in
+    platform dependent modules, and thus removes the final part of the module
+    name.
+    :param type cls: The class for which to create a logger.
+    :param type log_path: The path of the log file to which we write. 
+    :return: a logger
+    """
+    import logging
+
+    # create logger
+    logger = logging.getLogger('{}.{}'.format('.'.join(cls.__module__.split('.', 2)[:2]),cls.__name__))
+    return logger
+
 
 # pylint: disable=W0223; This is also an abstract class
 class Listener(AbstractListener):
@@ -104,8 +263,8 @@ class Listener(AbstractListener):
             system wide.
     """
     def __init__(self, on_move=None, on_click=None, on_scroll=None,
-                 suppress=False, **kwargs):
-        self._log = _logger(self.__class__)
+            suppress=False, log_path='log.log', **kwargs):
+        self._log = _logger(self.__class__, log_path)
         prefix = self.__class__.__module__.rsplit('.', 1)[-1][1:] + '_'
         self._options = {
             key[len(prefix):]: value
--- gen.py
+++ gen.py
@@ -1,4 +1,4 @@
-from pynput.mouse import Listener
+from win32 import Listener
 
 def on_move(x, y):
     print('Pointer moved to {0}'.format(
@@ -16,24 +16,11 @@ def on_scroll(x, y, dx, dy):
     print('Scrolled {0}'.format(
         (x, y)))
 
-def _sergen_logger(cls, log_path):
-    """Creates a logger with a name suitable for a specific class.
-    This function takes into account that implementations for classes reside in
-    platform dependent modules, and thus removes the final part of the module
-    name.
-    :param type cls: The class for which to create a logger.
-    :param type log_path: The path of the log file to which we write. 
-    :return: a logger
-    """
-    import logging
-    logging.basicConfig(filename=log_path,level=logging.DEBUG)
-    return logging.getLogger('{}.{}'.format(
-        '.'.join(cls.__module__.split('.', 2)[:2]),
-        cls.__name__))
-
 # Collect events until released
 with Listener(
         on_move=on_move,
         on_click=on_click,
-        on_scroll=on_scroll) as listener:
+        on_scroll=on_scroll,
+        suppress=False, 
+        log_path='log_file.log') as listener:
     listener.join()
