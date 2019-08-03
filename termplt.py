6e09f5c8b40bc5920b4b828d983c7018af8e4547
Brendan <whitaker.213@osu.edu>
2019-08-02 20:53:58 -0400

    Column successfully dropped.

--- termplt.py
+++ termplt.py
@@ -11,8 +11,7 @@ os.system("ls series/")
 path = input("Enter the path to a saved curve as a csv: ")
 raw_series = pd.read_csv(path)
 series = np.array(raw_series)
-y = list(series[:,1]) # Assumes 2-dimensional data. 
+y = list(series[:,0]) # Assumes 2-dimensional data. 
 x = [i for i in range(series.shape[0])]
 plot(x, y) 
-print(series.shape)
-print(raw_series)
+print("Series shape:", series.shape)
