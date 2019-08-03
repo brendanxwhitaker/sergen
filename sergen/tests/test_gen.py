126cc6af40cfa97e5952a914a49cb5ba3d1b60a6
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-03 10:18:49 -0400

    First test written.

---
+++ sergen/tests/test_gen.py
@@ -0,0 +1,30 @@
+import pytest
+from unittest import TestCase
+from sergen import gen
+
+coord_list = []
+index = 0
+start = 0
+end = 0
+
+class TestGen(TestCase):
+    # Globals.
+    global coord_list
+    global index
+    global start
+    global end
+
+    def test_on_move(self):
+        global coord_list
+        global index
+        global start
+        global end
+        coord_list = []
+        index = 0
+        start = 0
+        end = 0
+
+        on_move(0,0)
+        assert coord_list = [[0,0]] 
+        
+    
