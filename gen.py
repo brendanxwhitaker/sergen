1f9b918fc47e619e09285b4f1df7fb1d9090331d
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-02 20:51:27 -0400

    Drop column test finished.

--- gen.py
+++ gen.py
@@ -114,5 +114,6 @@ while raw_steps > TIME_STEPS:
 name = input("Enter a path for the saved file (include `.csv`): ")
 coords_df = pd.DataFrame(coords)
 coords_df.columns = ['x', 'y']
-coords_df.drop('x', 0)
+coords_df = coords_df.drop(['x'], 1)
 coords_df.to_csv(name, index=False)
+print(coords_df)
