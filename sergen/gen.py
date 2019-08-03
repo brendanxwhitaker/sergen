cb85b63bb10833cddd3bf1aa3a58f126c95bc2a5
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-03 12:23:30 -0400

    utils added.

--- sergen/gen.py
+++ sergen/gen.py
@@ -69,34 +69,8 @@ raw_steps = coords.shape[0]
 coords = coords.astype(float)
 
 if RESHAPE:
+    coords = resize(coords, raw_steps, REPEAT)
 
-def resize(coords: np.ndarray:, raw_steps: int) -> np.ndarray:
-    """Resizes first dim of coordinate array. """
-    # Interpolate time series. 
-    if REPEAT:
-        full_reps = TIME_STEPS // raw_steps
-        reps = [coords] * full_reps
-        coords = np.concatenate(reps)
-        raw_steps = coords.shape[0]
-
-    while REPEAT and raw_steps < TIME_STEPS:
-        diff = TIME_STEPS - raw_steps
-        coords = np.concatenate([coords, coords[:diff]])
-        raw_steps = coords.shape[0]
-
-    while not REPEAT and raw_steps < TIME_STEPS:
-        add_loc = random.randint(0,raw_steps - 2) # Inclusive right index. 
-        p1 = coords[add_loc]
-        p2 = coords[add_loc + 1]
-        mean = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
-        coords = np.insert(coords, add_loc + 1, mean, 0)
-        raw_steps = coords.shape[0]
-
-    # Filter time series. 
-    while raw_steps > TIME_STEPS:
-        del_loc = random.randint(0,raw_steps - 1)
-        coords = np.delete(coords, del_loc, 0)
-        raw_steps = coords.shape[0]
 
 name = input("Enter a path for the saved file (include `.csv`): ")
 coords_df = pd.DataFrame(coords)
