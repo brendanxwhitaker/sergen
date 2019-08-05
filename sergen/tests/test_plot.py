7114e2e217f7cc9dafd7e54f00647b2c333f84e2
Brendan <whitaker.213@osu.edu>
2019-08-05 09:56:32 -0400

    Listener tests fixed.

---
+++ sergen/tests/test_plot.py
@@ -0,0 +1,19 @@
+import pytest
+import numpy as np
+from unittest import TestCase
+
+from sergen import gen, listener
+
+coord_list = []
+index = 0
+start = 0
+end = 0
+
+class TestListener(TestCase):
+    """
+    error_text = "Found no compatible display."
+    try:
+        listener.listen(gen.on_move, gen.on_click)
+    except OSError as e:
+        assert str(e) == error_text
+    """
