240fbb1c43069c64241f9193efc0334478290dca
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-08-05 09:56:32 -0400

    Listener tests fixed.

--- sergen/tests/test_listener.py
+++ sergen/tests/test_listener.py
@@ -10,8 +10,9 @@ start = 0
 end = 0
 
 class TestListener(TestCase):
-    error_text = "Found no compatible display."
-    try:
-        listener.listen(gen.on_move, gen.on_click)
-    except OSError as e:
-        assert str(e) == error_text
+    def test_listen(self):
+        error_text = "Found no compatible display."
+        try:
+            listener.listen(gen.on_move, gen.on_click)
+        except OSError as e:
+            assert str(e) == error_text
