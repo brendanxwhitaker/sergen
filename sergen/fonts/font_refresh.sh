c7575577efd4a536c3bfeecb63e210c0e48f7c96
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-10-09 15:27:10 -0400

    Dirty commit.

--- sergen/fonts/font_refresh.sh
+++ sergen/fonts/font_refresh.sh
@@ -1,2 +1,6 @@
 sudo fc-cache -fv
 rm -fr ~/.cache/matplotlib
+
+# RHEL:
+# Copy to ~/.fonts/
+# fc-cache ~/.fonts/
--- sergen/termplt.py
+++ sergen/termplt.py
@@ -5,14 +5,24 @@ import numpy as np
 import pandas as pd
 from terminalplot import plot
 
-assert platform.system() == 'Linux'
-assert os.path.isdir("series/")
-print("The following files are available to print (by entering `series/<name>.csv`):")
-os.system("ls series/")
-PATH = input("Enter the path to a saved curve as a csv: ")
-RAW_SERIES = pd.read_csv(PATH)
-SERIES = np.array(RAW_SERIES)
-y = list(SERIES[:, 0])     # Assumes 2-dimensional data.
-x = [i for i in range(SERIES.shape[0])]
-plot(x, y)
-print("Series shape:", SERIES.shape)
+
+def plot_to_terminal(series: np.ndarray) -> None:
+    """ Plots the first dimension of a ``np.ndarray`` to the terminal. """
+    assert platform.system() == "Linux"
+    y = list(series[:, 0])  # Assumes 2-dimensional data.
+    x = [i for i in range(series.shape[0])]
+    plot(x, y)
+    print("Series shape:", series.shape)
+
+
+if __name__ == "__main__":
+    assert platform.system() == "Linux"
+    assert os.path.isdir("series/")
+    print(
+        "The following files are available to print (by entering `series/<name>.csv`):"
+    )
+    os.system("ls series/")
+    PATH = input("Enter the path to a saved curve as a csv: ")
+    RAW_SERIES = pd.read_csv(PATH)
+    SERIES = np.array(RAW_SERIES)
+    plot_to_terminal(SERIES)
